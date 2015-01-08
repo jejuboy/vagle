ce8fb86ad3075e8c1b40d749170cf60110a5b470
jejuboy <totaldesigner@hotmail.com>
2015-01-09 08:51:02 +0900

    create

---
+++ Vagrantfile
@@ -0,0 +1,9 @@
+Vagrant.configure(2) do |config| 
+	config.vm.box = "ubuntu-14.04.1-desktop-amd64"
+	config.vm.box_url = "boxes/ubuntu-14.04.1-desktop-amd64.box"
+	config.vm.provider :virtualbox do |vb|
+      vb.name = "vagle"
+	  vb.gui = true
+	end
+	#config.vm.provision "shell", path: "bootstrap.sh"
+end
---
+++ bootstrap.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#apt-get update
+#apt-get install -y git
+#apt-get install -y python-software-properties
+#apt-add-repository -y ppa:ansible/ansible
+#apt-get update
+#apt-get install -y ansible
+#cd /home/vagrant
+#git clone https://github.com/jejuboy/vagle.git
+#chown -R vagrant:vagrant vagle
+#cd vagle
+#ansible-playbook -i hosts site.yml
