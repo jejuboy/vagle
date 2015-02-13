38ca58840c5fe62d74a269ba79bb4728c0802e3a
jejuboy <totaldesigner@gmail.com>
2015-02-13 14:25:14 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,9 +3,9 @@
 # Install Git
 sudo apt-get update
 sudo apt-get install -y git
-sudo apt-get install -y python-software-properties
 
 # Install Ansible
+sudo apt-get install -y python-software-properties
 sudo apt-add-repository -y ppa:ansible/ansible
 sudo apt-get update
 sudo apt-get install -y ansible
